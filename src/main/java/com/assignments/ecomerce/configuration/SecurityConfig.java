097f3ad6c8eab9c08a8e7c03676c21ad203ffa5b
Nguyen Chi Cong <chicongkhpro@gmail.com>
2023-12-07 19:37:52 +0700

    update

--- src/main/java/com/assignments/ecomerce/configuration/SecurityConfig.java
+++ src/main/java/com/assignments/ecomerce/configuration/SecurityConfig.java
@@ -25,7 +25,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(c -> c.disable())
-                .authorizeHttpRequests(request -> request.requestMatchers("/statistical")
+                .authorizeHttpRequests(request -> request.requestMatchers("/index")
                         .hasAuthority("ADMIN")
 /*                        .requestMatchers("/indexUser").hasAuthority("USER")*/
                         .requestMatchers("/search-customer-ByEmployee/0?keyword=").hasAuthority("EMPLOYEE")
